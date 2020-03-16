e77cb0e42f7be718e5d1d623a3602884c114b22f
marcomoauro <marco@wanderio.com>
2020-03-16 17:12:40 +0100

    improvements

--- TrainTestFiles.py
+++ TrainTestFiles.py
@@ -15,22 +15,34 @@ def config():
 def files(config):
 	train_files = []
 	test_files = []
-	train_files_size = int(float(config['train_perc']) * int(config['txts_size']))
+	txts_size = calculate_txts_size()
+	train_files_size = int(float(config['train_perc']) * txts_size)
+	data_dir = os.getcwd() + "/data/"
+	files = os.listdir(data_dir)
+	random.shuffle(files)
+	count = 0
+	for file in files:
+		number, ext = file.split('.')
+		if ext == 'jpg':
+			continue
+		count += 1
+		if count <= train_files_size:
+			train_files.append(file)
+		else:
+			test_files.append(file)
+	write_file(train_files, 'train.txt')
+	write_file(test_files, 'test.txt')
+
+def calculate_txts_size():
 	data_dir = os.getcwd() + "/data/"
 	txts = os.listdir(data_dir)
-	random.shuffle(txts)
 	count = 0
 	for txt in txts:
 		number, ext = txt.split('.')
 		if ext == 'jpg':
 			continue
 		count += 1
-		if count <= train_files_size:
-			train_files.append(txt)
-		else:
-			test_files.append(txt)
-	write_file(train_files, 'train.txt')
-	write_file(test_files, 'test.txt')
+	return count
 
 def write_file(files, file_name):
 	file_name_path = os.getcwd()+"/data/"+file_name
